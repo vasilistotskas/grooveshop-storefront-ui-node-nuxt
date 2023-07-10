6dde62d49e226c87e31d28fe16d628f3a5078475
Gro0ve <vassilistotskas@msn.com>
2023-07-10 17:22:41 +0300

    fix(workflows): Trying to push to docker

--- config/nitro.ts
+++ config/nitro.ts
@@ -1,8 +1,6 @@
 export const nitro = {
 	compressPublicAssets: true,
 	prerender: {
-		crawlLinks: true,
-		ignore: [],
-		routes: ['/', '/200.html', '/404.html']
+		crawlLinks: false
 	}
 }
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	apps: [
 		{
-			name: 'NuxtAppName',
+			name: '@vasilistotskas/grooveshop-storefront-ui-node-nuxt',
 			port: '3000',
 			exec_mode: 'cluster',
 			instances: 'max',
