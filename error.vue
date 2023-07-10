bcef1e4c5f63f52d84fd175254c0452e81bc72d2
Gro0ve <vassilistotskas@msn.com>
2023-07-10 12:23:27 +0300

    fix(workflows): Trying to push to docker

--- error.vue
+++ error.vue
@@ -5,7 +5,7 @@
 			There was an error 😱
 
 			<br />
-			<button @click="handleError">Clear error</button>
+			<button type="button" @click="handleError">Clear error</button>
 			<br />
 			<NuxtLink to="/404"> Trigger another error </NuxtLink>
 			<br />
@@ -15,7 +15,8 @@
 </template>
 
 <script lang="ts" setup>
-import { FetchError } from 'ofetch'
+import { FetchError } from "ofetch";
+import { ITheme } from "~/utils/theme";
 
 const props = defineProps({
 	error: {
@@ -23,6 +24,35 @@ const props = defineProps({
 		required: false,
 		default: null
 	}
+})
+
+const route = useRoute()
+const router = useRouter()
+
+const theme = useState<ITheme>('theme.current')
+const themeClass = computed(() => (theme.value === 'dark' ? 'dark' : 'light'))
+
+const i18nHead = useLocaleHead({
+	addDirAttribute: true,
+	addSeoAttributes: true,
+	identifierAttribute: 'hid',
+	route,
+	router,
+	i18n: useI18n()
+})
+useHead({
+  title: 'Error',
+	htmlAttrs: {
+		lang: i18nHead.value.htmlAttrs!.lang,
+		class: () => themeClass.value,
+		dir: i18nHead.value.htmlAttrs?.dir
+	},
+	link: [...(i18nHead.value.link || [])],
+	meta: [...(i18nHead.value.meta || [])]
+})
+useServerSeoMeta({
+  title: 'Error',
+  description: 'Error page',
 })
 
 const handleError = () => clearError({ redirect: '/' })
