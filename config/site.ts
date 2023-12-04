be6ea19a48dc79de427dacf9776822cab7c4444b
Gro0ve <vassilistotskas@msn.com>
2023-12-04 13:48:57 +0200

    feat: Bump versions.
    - Add try catch in `auth` plugin

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export const site = {
-	url: process.env.NUXT_PUBLIC_SITE_URL,
-	name: process.env.NUXT_PUBLIC_SITE_NAME,
-	description: process.env.NUXT_PUBLIC_SITE_DESCRIPTION,
-	defaultLocale: process.env.NUXT_PUBLIC_DEFAULT_LOCALE
+	url: process.env.NUXT_PUBLIC_SITE_URL || 'https://grooveshop.site',
+	name: process.env.NUXT_PUBLIC_SITE_NAME || 'Grooveshop',
+	description: process.env.NUXT_PUBLIC_SITE_DESCRIPTION || 'Grooveshop',
+	defaultLocale: process.env.NUXT_PUBLIC_DEFAULT_LOCALE || 'en'
 }
--- plugins/auth.ts
+++ plugins/auth.ts
@@ -1,46 +1,59 @@
 export default defineNuxtPlugin(async (NuxtApp) => {
-	try {
-		const initialized = useState('auth-initialized', () => false)
+	const initialized = useState('auth-initialized', () => false)
 
-		const idb = await useAsyncIDBKeyval('auth', false)
-		const { _loggedIn, _totpActive, _totpAuthenticated } = useAuthSession()
+	const idb = await useAsyncIDBKeyval('auth', false)
+	const { _loggedIn, _totpActive, _totpAuthenticated } = useAuthSession()
 
-		if (!initialized.value) {
-			const { fetchUser } = useAuth()
-			const { _refreshToken, _accessToken, _refresh } = useAuthSession()
+	if (!initialized.value) {
+		const { fetchUser } = useAuth()
+		const { _refreshToken, _accessToken, _refresh } = useAuthSession()
 
-			if (_accessToken.get()) {
+		if (_accessToken.get()) {
+			try {
 				await fetchUser()
-			} else {
-				const isLoggedIn = _loggedIn.get()
+			} catch (e) {
+				// eslint-disable-next-line no-console
+				console.error(e)
+				return
+			}
+		} else {
+			const isLoggedIn = _loggedIn.get()
 
-				if (isLoggedIn || _refreshToken.get()) {
+			if (isLoggedIn || _refreshToken.get()) {
+				try {
 					await _refresh()
-					if (_accessToken.get()) {
+				} catch (e) {
+					// eslint-disable-next-line no-console
+					console.error(e)
+					return
+				}
+				if (_accessToken.get()) {
+					try {
 						await fetchUser()
+					} catch (e) {
+						// eslint-disable-next-line no-console
+						console.error(e)
+						return
 					}
 				}
 			}
 		}
+	}
 
-		initialized.value = true
+	initialized.value = true
 
-		const { user } = useAuthSession()
-		const loggedIn = useState<boolean>('auth-loggedIn')
+	const { user } = useAuthSession()
+	const loggedIn = useState<boolean>('auth-loggedIn')
 
-		if (user.value) {
-			_loggedIn.set(true)
-			loggedIn.value = true
-			idb.value = true
-		} else {
-			_loggedIn.set(false)
-			_totpActive.set('false')
-			_totpAuthenticated.set('false')
-			loggedIn.value = false
-			idb.value = false
-		}
-	} catch (e) {
-		// eslint-disable-next-line no-console
-		console.error('Something went wrong while initializing auth plugin', e)
+	if (user.value) {
+		_loggedIn.set(true)
+		loggedIn.value = true
+		idb.value = true
+	} else {
+		_loggedIn.set(false)
+		_totpActive.set('false')
+		_totpAuthenticated.set('false')
+		loggedIn.value = false
+		idb.value = false
 	}
 })
