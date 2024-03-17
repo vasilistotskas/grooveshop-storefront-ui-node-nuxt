0df68994a043649876877490385d9c5358bd0528
vasilistotskas <vassilistotskas@msn.com>
2024-03-17 20:40:50 +0200

    fix: remove routes from sw.ts

--- service-worker/sw.ts
+++ service-worker/sw.ts
@@ -1,11 +1,7 @@
 /// <reference lib="WebWorker" />
 /// <reference types="vite/client" />
-import { CacheableResponsePlugin } from 'workbox-cacheable-response'
 import { clientsClaim } from 'workbox-core'
-import { ExpirationPlugin } from 'workbox-expiration'
 import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
-import { registerRoute } from 'workbox-routing'
-import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies'
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -18,38 +14,6 @@ precacheAndRoute(entries)
 
 // clean old assets
 cleanupOutdatedCaches()
-
-registerRoute(
-  ({ request, sameOrigin }) => sameOrigin && request.destination === 'manifest',
-  new NetworkFirst({
-    cacheName: 'grooveshop-webmanifest',
-    plugins: [
-      new CacheableResponsePlugin({ statuses: [200] }),
-      new ExpirationPlugin({ maxEntries: 100 }),
-    ],
-  }),
-)
-registerRoute(
-  ({ request, url }) => {
-    const allowedOrigins = [
-      'http://localhost:3003',
-      'https://assets.grooveshop.site',
-    ]
-    return (
-      allowedOrigins.includes(url.origin) && request.destination === 'image'
-    )
-  },
-  new StaleWhileRevalidate({
-    cacheName: 'media-stream',
-    plugins: [
-      new CacheableResponsePlugin({ statuses: [0, 200] }),
-      new ExpirationPlugin({
-        maxAgeSeconds: 60 * 60 * 24 * 15,
-        maxEntries: 200,
-      }),
-    ],
-  }),
-)
 
 self.skipWaiting()
 clientsClaim()
