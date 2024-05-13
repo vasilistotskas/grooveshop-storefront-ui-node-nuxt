d55fcf3944916d33a31c65d94b2d25912e8d596a
vasilistotskas <vassilistotskas@msn.com>
2024-05-14 01:10:55 +0300

    fix: remove some route-rules

--- config/route-rules.ts
+++ config/route-rules.ts
@@ -1,11 +1,4 @@
 export const routeRules = {
-  '/**': process.client
-    ? {}
-    : { cache: { swr: true, maxAge: 120, staleMaxAge: 60, headersOnly: true } },
-  '/products': { swr: true },
-  '/products/**': { swr: 3600 },
-  '/blog': { isr: 3600 },
-  '/blog/**': { isr: true },
   '/api/**': { cors: true },
   '/manifest.webmanifest': {
     headers: {
