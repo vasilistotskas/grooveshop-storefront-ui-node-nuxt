17881d274dadc536491f0433471bf6b17692a902
vasilistotskas <vassilistotskas@msn.com>
2024-05-13 18:17:08 +0300

    fix: lang update and add headers for some public routes

--- config/route-rules.ts
+++ config/route-rules.ts
@@ -17,20 +17,33 @@ export const routeRules = {
     cache: {
       maxAge: 60 * 60 * 24 * 365,
     },
+    headers: {
+      'Content-Type': 'image/x-icon',
+      'Cache-Control': 'max-age=31536000',
+    },
   },
   '/favicon/**': {
     cache: {
       maxAge: 60 * 60 * 24 * 365,
+    },
+    headers: {
+      'Cache-Control': 'max-age=31536000',
     },
   },
   'img/**': {
     cache: {
       maxAge: 60 * 60 * 24 * 365,
     },
+    headers: {
+      'Cache-Control': 'max-age=31536000',
+    },
   },
   '/screenshots/**': {
     cache: {
       maxAge: 60 * 60 * 24 * 365,
+    },
+    headers: {
+      'Cache-Control': 'max-age=31536000',
     },
   },
 }
