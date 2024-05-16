03162df91107d860f2b20cba42e8e7a168131fd9
vasilistotskas <vassilistotskas@msn.com>
2024-05-16 11:31:45 +0300

    fix: Include forgotten endpoint

--- server/api/blog/posts/index.get.ts
+++ server/api/blog/posts/index.get.ts
@@ -5,10 +5,15 @@ import { ZodPagination } from '~/types/pagination'
 
 export default defineEventHandler(async (event: H3Event) => {
   const config = useRuntimeConfig()
-  const query = await getValidatedQuery(event, ZodBlogPostQuery.parse)
-  const url = buildFullUrl(`${config.public.apiBaseUrl}/blog/post`, query)
-  const response = await $fetch(url, {
-    method: 'GET',
-  })
-  return await parseDataAs(response, ZodPagination(ZodBlogPost))
+  try {
+    const query = await getValidatedQuery(event, ZodBlogPostQuery.parse)
+    const url = buildFullUrl(`${config.public.apiBaseUrl}/blog/post`, query)
+    const response = await $fetch(url, {
+      method: 'GET',
+    })
+    return await parseDataAs(response, ZodPagination(ZodBlogPost))
+  }
+  catch (error) {
+    await handleError(error)
+  }
 })
