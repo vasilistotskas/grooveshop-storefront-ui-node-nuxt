208e598199d968dfe67afa29ceeed4503a2adad6
vasilistotskas <vassilistotskas@msn.com>
2024-10-04 14:49:13 +0300

    feat: Bump Versions and build manifest hook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,16 @@ export default defineNuxtConfig({
   features: {
     inlineStyles: true,
   },
+  hooks: {
+    'build:manifest': (manifest) => {
+      const css = manifest['node_modules/nuxt/dist/app/entry.js']?.css
+      if (css) {
+        for (let i = css.length - 1; i >= 0; i--) {
+          if (css[i]?.startsWith('entry')) css.splice(i, 1)
+        }
+      }
+    },
+  },
   app: {
     head: {
       viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
