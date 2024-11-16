24393a7ebdb6bf506f8859d130d7af3552f377cd
vasilistotskas <vassilistotskas@msn.com>
2024-11-16 20:59:38 +0200

    fix(middleware): redirect

--- server/middleware/redirects.ts
+++ server/middleware/redirects.ts
@@ -1,10 +1,7 @@
 export default defineEventHandler((event) => {
   const host = getRequestHost(event)
-
+  // Redirect www to non-www
   if (host.startsWith('www.')) {
-    const nonWwwHost = host.slice(4)
-    const protocol = event.node.req.headers['x-forwarded-proto'] || 'https'
-    const newUrl = `${protocol}://${nonWwwHost}${event.node.req.url}`
-    return sendRedirect(event, newUrl, 301)
+    return sendRedirect(event, `https://${host.slice(4)}${event.path}`, 301)
   }
 })
