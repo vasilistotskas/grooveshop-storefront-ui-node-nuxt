7868e0887172f8916cd9300c4e06577b785411c9
vasilistotskas <vassilistotskas@msn.com>
2024-12-11 01:30:04 +0200

    feat: add breadcrumb

--- app/pages/account/signup/passkey/index.vue
+++ app/pages/account/signup/passkey/index.vue
@@ -1,5 +1,25 @@
 <script lang="ts" setup>
 const { t } = useI18n({ useScope: 'local' })
+const localePath = useLocalePath()
+
+const links = computed(() => [
+  {
+    to: localePath('index'),
+    label: t('breadcrumb.items.index.label'),
+    icon: t('breadcrumb.items.index.icon'),
+  },
+  {
+    to: localePath('account-signup'),
+    label: t('breadcrumb.items.account-signup.label'),
+    icon: t('breadcrumb.items.account-signup.icon'),
+  },
+  {
+    to: localePath('account-signup-passkey'),
+    label: t('breadcrumb.items.account-signup-passkey.label'),
+    icon: t('breadcrumb.items.account-signup-passkey.icon'),
+    current: true,
+  },
+])
 
 useSeoMeta({
   title: t('title'),
@@ -17,13 +37,20 @@ definePageMeta({
 <template>
   <PageWrapper
     class="
-      container-3xs flex flex-col gap-4 !p-0
+      container-3xs flex flex-col gap-4 md:!p-0
 
       md:gap-8
     "
   >
+    <UBreadcrumb
+      :links="links"
+      :ui="{
+        li: 'text-primary-950 dark:text-primary-50',
+        base: 'text-xs md:text-md',
+      }"
+      class="container-3xs relative mb-5 min-w-0"
+    />
     <PageTitle :text="t('title')" class="text-center" />
-
     <AccountSignupPasskeyForm />
   </PageWrapper>
 </template>
@@ -31,4 +58,12 @@ definePageMeta({
 <i18n lang="yaml">
 el:
   title: Εγγραφή με κωδικό μιας χρήσης
+  breadcrumb:
+    items:
+      account-signup:
+        label: Εγγραφή
+        icon: i-heroicons-arrow-right-on-rectangle
+      account-signup-passkey:
+        label: Κλειδί
+        icon: i-heroicons-key
 </i18n>
