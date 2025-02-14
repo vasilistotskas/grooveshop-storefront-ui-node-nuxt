abebf8f645137ec0de66b84577c7d148ef425e57
Vasilis Totskas <vassilistotskas@msn.com>
2025-02-14 23:52:05 +0200

    feat: remove health check api call, Bump Versions

--- app/plugins/0.setup.ts
+++ app/plugins/0.setup.ts
@@ -2,20 +2,10 @@ export default defineNuxtPlugin({
   name: 'setup',
   parallel: true,
   async setup() {
-    const appStore = useAppStore()
-    const { healthCheck } = appStore
-    const { status } = await healthCheck()
-
-    if (status.value === 'error') {
-      return
-    }
-
     const authStore = useAuthStore()
     const { setupConfig, setupSession } = authStore
 
-    await Promise.all([
-      setupConfig(),
-      setupSession(),
-    ])
+    await setupConfig()
+    await setupSession()
   },
 })
